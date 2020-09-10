2e42a715db503a3eb305efb692995e2f570db717
Jorge Rangel Jimenez <georgerangelcode@gmail.com>
2020-09-10 07:12:47 -0500

    Renderizado Condicional de imagenes en vista de detalle

--- src/pages/StoreDetail.js
+++ src/pages/StoreDetail.js
@@ -1,10 +1,15 @@
 import React, { useContext } from "react";
 import Context from "../Context";
 import { Store } from "../components/Store";
-import { Redirect } from "react-router-dom";
-import panosPizza from "../assets/images/Panos_pizza.png";
-import "../styles/StoreDetail.scss";
 import { Header } from "../components/Header";
+import { Redirect } from "react-router-dom";
+import "../styles/StoreDetail.scss";
+import panos from "../assets/images/panos_pizza_1.png";
+import trulli from "../assets/images/trulli_pizza.png";
+import stroller from "../assets/images/stroller_pizza_1.png";
+import sbarro from "../assets/images/sbarro_pizza.png";
+import camion from "../assets/images/pizzeria_camion_pizza.png";
+import vogliadipizza from "../assets/images/vogliadipizza_pizza.png";
 
 export const StoreDetail = ({ location }) => {
   const id = location.pathname.substring(
@@ -32,11 +37,28 @@ export const StoreDetail = ({ location }) => {
         <ul className="list-group">
           {storeDetail.products.map((product) => (
             <li key={product.id} className="list-group-item">
-              <img
-                src={panosPizza}
-                alt="Under the Dome"
-                className="product__image"
-              />
+              {product.name === "Pizza de Pollo" && (
+                <img src={panos} alt="panos" className="product__image" />
+              )}
+              {product.name === "Piza Pollo y Champiñones" && (
+                <img src={trulli} alt="trulli" className="product__image" />
+              )}
+              {product.name === "Pizza Vegetales" && (
+                <img src={stroller} alt="stroller" className="product__image" />
+              )}
+              {product.name === "Pizza Vegetales y Atun " && (
+                <img src={sbarro} alt="sbarro" className="product__image" />
+              )}
+              {product.name === "Pizza 3 Quesos con Champiñones" && (
+                <img src={camion} alt="camion" className="product__image" />
+              )}
+              {product.name === "Pizza Jamon y Queso" && (
+                <img
+                  src={vogliadipizza}
+                  alt="vogliadipizza"
+                  className="product__image"
+                />
+              )}
               {product.name}
             </li>
           ))}
